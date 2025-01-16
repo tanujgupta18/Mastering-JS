b297ba897c6e503f8e838b0fa399aa46ad0136cb
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-01-16 18:23:08 +0530

    Arranged The stuff

---
+++ Practise/Practise.js
@@ -0,0 +1,19 @@
+// let stringVar = "Hello";
+// let numberVar = 5;
+// let boolVar = true;
+// let undefined;
+// let nullVar = null;
+
+// let x;
+// console.log(x);
+
+function checkEvenOdd(n) {
+  if (n % 2 === 0) {
+    console.log("Even Number");
+  } else {
+    console.log("Odd Number");
+  }
+}
+
+checkEvenOdd(5);
+checkEvenOdd(10);
