4ed90d659c01a36c76efd93a012f241ba36885f4
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-01-26 19:29:15 +0530

    Create PractiseSet7.js

---
+++ Practise/PractiseSet7.js
@@ -0,0 +1,67 @@
+// Quesition: 1
+// let arr = [5, 6];
+// arr.push(7);
+// arr.unshift(4);
+// console.log(arr);
+
+// Quesition: 2
+// function getElement(arr, position) {
+//   return arr[position - 1];
+// }
+// let arr = [1, 2, 4, 12, 5, 7];
+// console.log(getElement(arr, 3));
+
+// Question: 3;
+// function arrCopy(arr) {
+//   return arr.slice(1, 4);
+// }
+// let arr = [2, 3, 5, 10, 6];
+// console.log(arrCopy(arr));
+
+// Question: 4
+// function isPrime(num) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// let num = 2;
+// let i = 0;
+// while (true) {
+//   if (isPrime(num)) {
+//     console.log(num);
+//     i++;
+//     if (i >= 5) {
+//       break;
+//     }
+//   }
+//   num++;
+// }
+
+// Question: 5
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+// for (let i = 0; i < 10; i++) {
+//   console.log(10 - i);
+// }
+
+// Question: 6
+// let nums = [1, -6, 5, 7, -98];
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] < 0) continue;
+//   console.log(nums[i]);
+// }
+
+// Question: 7;
+// let arr = ["JavaScript", "is", "a", "best", "language"];
+// let result = "";
+// for (let i = 0; i < arr.length; i++) {
+//   result += arr[i] + " ";
+// }
+// console.log(result);
+
+// console.log(arr.join(" "));
