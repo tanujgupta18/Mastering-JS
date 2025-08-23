2037788062c9a619aa2198947446c0deab6b437b
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-08-23 12:37:02 +0530

    Practise 2.0

---
+++ Practise 2.0/Practise Set - 1.js
@@ -0,0 +1,20 @@
+let a = "Hello";
+let b = 5;
+console.log(a + b);
+
+console.log(typeof (a + b));
+
+const c = {
+  name: "Harry",
+  age: "15",
+};
+
+console.log(c);
+
+// Access Syntax 1
+c["name"] = "Rohit";
+console.log(c);
+
+// Access Syntax 2
+c.name = "Mayank";
+console.log(c);
